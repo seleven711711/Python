081789892cfcf0cbf3fcab54fc527919d7858dad
Андрей Боев <a.boev.it@gmail.com>
2025-10-21 19:16:13 +0300

    Проверка ДЗ

--- 3. Условные конструкции/Задания/Грибы.py
+++ 3. Условные конструкции/Задания/Грибы.py
@@ -1,7 +1,8 @@
 n = int(input())
-if n % 10 == 1 and n %100 != 11:
-    print('гриб')
-elif 1 < n % 10 < 5 and 14<n<11:
-    print('гриба')
-else:
-    print('грибов')
+for i in range(1000):
+    if n % 10 == 1 and n % 100 != 11:
+        print('гриб')
+    elif 1 < n % 10 < 5 and n % 100 < 12:
+        print('гриба')
+    else:
+        print('грибов')
