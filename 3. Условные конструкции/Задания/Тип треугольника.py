9314eba65a410b723edd6969f102c4c4fb06d871
Гоша <georgyevseev7775@gmail.com>
2025-10-25 13:51:32 +0300

    Решение задач

---
+++ 3. Условные конструкции/Задания/Тип треугольника.py
@@ -0,0 +1,17 @@
+a = int(input())
+b = int(input())
+c = int(input())
+k1 = min(a, b, c)
+h = max(a, b, c)
+k2 = a + b + c - (h + k1)
+if k1+k2<=h:
+    print('Не существует')
+elif k1**2+k2**2 == h**2:
+    print('Прямоугольный')
+elif k1**2+k2**2<h**2:
+    print('Остроугольный')
+elif k1**2+k2**2>h**2:
+    print('Тупоугольный')
+
+
+
