97861bb26bbbccdf25f0d7cb3d7efe86082bad50
Гоша <georgyevseev7775@gmail.com>
2025-10-31 20:09:40 +0300

    Обновлен статус задания 71 темы 7. Задание решено Верно

---
+++ ЕГЭ/Тема 7/Задания/+Задание 71.py
@@ -0,0 +1,16 @@
+# Решение
+
+
+
+
+
+
+
+answer = 65
+
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(7, 71, answer, 'fc490ca45c00b1249bbe3554a4fdf6fb'))
