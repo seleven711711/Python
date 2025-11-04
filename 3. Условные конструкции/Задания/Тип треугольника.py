945732e6f87215606ada97620395b90670967c13
Андрей Боев <a.boev.it@gmail.com>
2025-11-04 19:05:33 +0300

    NEW: Тема 11

--- 3. Условные конструкции/Задания/Тип треугольника.py
+++ 3. Условные конструкции/Задания/Тип треугольника.py
@@ -4,14 +4,11 @@ c = int(input())
 k1 = min(a, b, c)
 h = max(a, b, c)
 k2 = a + b + c - (h + k1)
-if k1+k2<=h:
+if k1 + k2 <= h:
     print('Не существует')
-elif k1**2+k2**2 == h**2:
+elif k1 ** 2 + k2 ** 2 == h ** 2:
     print('Прямоугольный')
-elif k1**2+k2**2<h**2:
+elif k1 ** 2 + k2 ** 2 < h ** 2:
     print('Остроугольный')
-elif k1**2+k2**2>h**2:
+elif k1 ** 2 + k2 ** 2 > h ** 2:
     print('Тупоугольный')
-
-
-
---
+++ 4. Циклы/4.1 Цикл for/main.py
@@ -0,0 +1,11 @@
+"""for c in 'Text':
+    print(c)
+"""
+
+"""
+print(list(range(10))) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(list(range(5, 10))) # [5, 6, 7, 8, 9]
+print(list(range(5, 15, 3))) # [5, 8, 11, 14]
+print(list(range(15, 4, -3))) # [15, 12, 9, 6]
+"""
+
