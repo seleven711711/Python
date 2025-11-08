a4035bdd474c322533c64c8ff866f159dd6873ef
Андрей Боев <a.boev.it@gmail.com>
2025-11-08 16:36:51 +0300

    NEW: Тема 2

---
+++ ЕГЭ/Тема 2/Задания/Задание 22.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+
+answer = ...
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(2, 22, answer, 'c5e4e768af58cf865c4006af69319e62'))
