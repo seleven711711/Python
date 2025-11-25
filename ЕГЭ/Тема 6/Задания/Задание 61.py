1e419e98e0e0bdb4f3bd304350020f63a9c25890
Андрей Боев <a.boev.it@gmail.com>
2025-11-25 19:13:52 +0300

    NEW: Тема 6

---
+++ ЕГЭ/Тема 6/Задания/Задание 61.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+
+answer = ...
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(6, 61, answer, 'aab3238922bcc25a6f606eb525ffdc56'))
