2a34463dda288e56c30c3aae2f587e8720f0ad97
Андрей Боев <a.boev.it@gmail.com>
2025-12-02 19:21:36 +0300

    NEW: Тема 9

---
+++ ЕГЭ/Тема 9/Задания/Задание 9.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+
+answer = ...
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(9, 9, answer, '812b4ba287f5ee0bc9d43bbf5bbe87fb'))
---
+++ ЕГЭ/Тема 9/Задания/Задание 91.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+
+answer = ...
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(9, 91, answer, '905056c1ac1dad141560467e0a99e1cf'))
