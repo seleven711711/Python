3d6a34f9efc23ba122721cc7ca7d23f7c2f6f17f
Андрей Боев <a.boev.it@gmail.com>
2026-01-13 19:53:28 +0300

    NEW: Тема 10 (Задания)

---
+++ ЕГЭ/Тема 10/Задания/Задание 103.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+
+answer = ...
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(10, 103, answer, 'd3d9446802a44259755d38e6d163e820'))
---
+++ ЕГЭ/Тема 10/Задания/Задание 104.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+
+answer = ...
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(10, 104, answer, 'c81e728d9d4c2f636f067f89cc14862c'))
