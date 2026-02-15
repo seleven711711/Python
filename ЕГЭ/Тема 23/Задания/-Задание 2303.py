167e1702a9a374702780c8f26fc7a4a92e848d78
Гоша <georgyevseev7775@gmail.com>
2026-02-15 15:08:14 +0300

    Обновлен статус задания № 2303 Тема: 23 > Неверно

---
+++ ЕГЭ/Тема 23/Задания/-Задание 2303.py
@@ -0,0 +1,23 @@
+# Решение
+def f(x,y):
+    if x==y:
+        return 1
+    if x > y or x==12 or x==15:
+        return 0
+    return f(x-1,y)*(x//2,y)*(x//3,y)
+print (f(19,1))
+
+
+
+
+
+
+
+
+answer =0
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(23, 2303, answer, '17e62166fc8586dfa4d1bc0e1742c08b'))
