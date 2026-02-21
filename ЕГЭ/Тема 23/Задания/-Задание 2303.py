8575f6f486db09fb31eda46c93160cbaf154e357
Гоша <georgyevseev7775@gmail.com>
2026-02-21 13:04:35 +0300

    Merge remote-tracking branch 'origin/main'

--- ЕГЭ/Тема 23/Задания/-Задание 2303.py
+++ ЕГЭ/Тема 23/Задания/-Задание 2303.py
@@ -4,7 +4,12 @@ def f(x,y):
         return 1
     if x > y or x==12 or x==15:
         return 0
-    return f(x-1,y)*f(x//2,y)*f(x//3,y)
+    if x%2==0:
+        return f(x // 2, y)+f(x-1,y)
+    elif x%3==0:
+        return f(x//3,y)+f(x-1,y)
+    else:
+        return f(x-1,y)
 print (f(19,1))
 
 
@@ -14,7 +19,7 @@ print (f(19,1))
 
 
 
-answer =0
+answer =...
 
 #
 
