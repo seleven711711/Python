aa0e18ccf41915a9cb8682cd739b47e47660115f
Гоша <georgyevseev7775@gmail.com>
2026-05-02 14:33:43 +0300

    Обновлен статус задания № 83 Тема: 8 > Верно

---
+++ ЕГЭ/Тема 8/Задания/+Задание 83.py
@@ -0,0 +1,22 @@
+# Решение
+from itertools import*
+
+print(list(product('бкфц',repeat=5))[238])
+
+
+
+
+
+
+
+
+
+
+
+answer = 'БЦФЦФ'
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(8, 83, answer, 'f0a551e113a7af86f780ae14e74c3ac7'))
